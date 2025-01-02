79c26943c7111ff6085ff2b95667ca125083d99e
Anmol Brar <anmol@brars.net>
2025-01-02 05:45:22 +0000

    feat: incl. routers and connect & disconnect to database on start/shutdown (#6)
    Reviewed-on: https://git.brars.net/tarangi/audio-gurbani-api/pulls/6
    Co-authored-by: Anmol Brar <anmol@brars.net>
    Co-committed-by: Anmol Brar <anmol@brars.net>

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import artist.views
+import audio.views
+import collection.views
+from database import database
 from fastapi import FastAPI
 
 app: FastAPI = FastAPI()
+
+app.include_router(router=artist.views.router, prefix="/api/v1")
+app.include_router(router=collection.views.router, prefix="/api/v1")
+app.include_router(router=audio.views.router, prefix="/api/v1")
+
+
+@app.on_event("startup")
+async def on_startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await database.disconnect()
